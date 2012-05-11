897e71740f0ab7ae0bdaab3d00d3bcc19650f125
Michael Kohanfars <michaelkohanfars@gmail.com>
2012-05-11 16:58:02 -0700

    ADD ALL THE FILES

---
+++ js/pict.js
@@ -0,0 +1,26 @@
+var pictureViewer = function(pictArray)
+                    {
+                        var picGallery = document.getElementById("cp");
+                        var list, anchor, img;
+                        
+                        for(var i = 0; i<pictArray.length; i++)
+                        {  
+                            //Create new img object
+                            img = document.createElement("img");
+                            img.src = "images/thumbs/"+pictArray[i];
+                            img.alt = pictArray[i];
+                            img.setAttribute("data-large","images/"+pictArray[i]);
+                            
+                            //create new anchor object
+                            anchor = document.createElement("a");
+                            anchor.href = "#";
+                            anchor.appendChild(img);
+                            
+                            //Create and append all items to the new list object
+                            list = document.createElement("li");
+                            list.appendChild(anchor);
+                            
+                            //Append everything to the main list
+                            picGallery.appendChild(list)
+                        }
+                    }
